0de90ae233e1572532a08c3cea8788651f27c84c
shanky <shanky.m.bharadwaj@gmail.com>
2024-01-21 04:36:07 +0530

    new commit.

--- app.py
+++ app.py
@@ -1,32 +1,71 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+import mysql.connector
+import base64
 import bcrypt
-import sqlite3
 
 app = Flask(__name__)
 
- #a get_db_connection function
-def get_db_connection():
-    connection = sqlite3.connect('carbon.db')
-    return connection
+# Database connection
+db_config = {
+    "host": "localhost",
+    "user": "shanky",
+    "password": "$hanky009",
+    "database": "carbon"
+}
 
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+@app.route('/')
+def index():
+    return render_template('choice.html')
+
+@app.route('/index')
+def next_page():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def sig():
+    return render_template('signin.html')
+
+
+# signup
+@app.route('/signup', methods=['POST'])
+def signup():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    username = request.form['username']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+
+    if password != confirm_password:
+        return "Password and Confirm Password do not match."
+
+    # Hash the password
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    cursor.execute("INSERT INTO signin1 (username, password) VALUES (%s, %s)", (username, hashed_password))
+    connection.commit()
+    return redirect('/signin')
+
+#signin
 @app.route('/signin', methods=['POST'])
 def signin():
     connection = get_db_connection()
     cursor = connection.cursor()
-
-    # The rest of your code for handling form submission
+    render_template('signin.html')
     username = request.form['username']
     password = request.form['password']
 
-    cursor.execute("SELECT * FROM signin WHERE username=%s", (username,))
+    cursor.execute("SELECT * FROM signin1 WHERE username=%s", (username,))
     user = cursor.fetchone()
 
     if user and user[2] and bcrypt.checkpw(password.encode('utf-8'), user[2].encode('utf-8')):
-        return render_template('index.html', username=username)
+        return render_template('dashboard.html', username=username)
 
     return "Invalid username or password."
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
